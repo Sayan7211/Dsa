4ecc3650e4eb78fb080b0a9eca4274bf529b33f1
Sayan7211 <96005220+Sayan7211@users.noreply.github.com>
2021-12-26 12:12:05 +0530

    Create c_queue.c

---
+++ c_queue.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#define max 5
+
+int choice,i,item;
+int r=-1,f=-1;
+int a[max];
+
+void insert()
+{
+	if(((r+1)%max)==f)
+	  printf("## Queue Is Full.\n");
+	  
+	else if(f=-1)
+	{
+		 r=0;
+		 f=0;
+		 a[r]=item;
+	}
+	else
+	{
+		printf("Enter Your Value To Be Insert: \n");
+		scanf("%d",&item);
+		
+		
+			
+			(r=(r+1)%max);
+		     a[r]=item;
+	}
+}
+
+void del()
+{
+	if(f==-1)
+	   printf("## Queue Is Empty.");
+	else if(f==r)
+	{
+		f=r=-1;
+	}	
+	else
+	{
+		(f=(f+1)%max);
+		
+	}
+} 
+
+void display()
+{
+        if(f==-1)
+            printf("\n## Queue Is Empty.");
+        else
+        {       
+		    for(i=f;i<r;i++)
+                printf("\nPosition:%d           Value:%d",i,a[i]);
+        }
+                printf("\n");
+}
+
+
+
+
+
+void main()
+{
+        printf("Simple Queue Programm.............\n");
+        while(1)
+        {
+                printf("\n##### Queue Main Manu #####\n");
+                printf("\n1.Insertion. \n2.Delection. \n3.Display \n4.Exit.\n");
+                printf("\nEnter Your Choice: ");
+                scanf("%d",&choice);
+        switch(choice)
+        {
+                case 1:
+                insert();
+                break;
+
+                case 2:
+                del();
+                break;
+
+                case 3:
+                display();
+                break;
+
+                case 4:
+                break;
+        }
+        }
+}
