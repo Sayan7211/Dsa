2733d78577ee50c37f62de8463668c8aa3fdeb79
Sayan7211 <96005220+Sayan7211@users.noreply.github.com>
2022-01-10 15:10:30 -0500

    Update c_queue.c

--- c_queue.c
+++ c_queue.c
@@ -1,63 +1,55 @@
 #include<stdio.h>
 #define max 5
 
-int choice,i,item;
+int i,item,choice;
 int r=-1,f=-1;
 int a[max];
 
-int insert()
+void insertion()
 {
-	if(((r+1)%max)==f)
-	  printf("## Queue Is Full.\n");
-	  
-	else if(f=-1)
-	{	printf("Enter Your Value To Be Insert: \n");
-		scanf("%d",&item);
-		 r=0;
-		 f=0;
-		 a[r]=item;
-	}
-	else
-	{
-		printf("Enter Your Value To Be Insert: \n");
-		scanf("%d",&item);
-		
-		
-			
-			(r=(r+1)%max);
-		     a[r]=item;
-	}
+        if(f==(r+1)%max)
+                printf("## Queue Is Fully.");
+        else
+        {
+                printf("Enter The Elements To Be Insert:  \n");
+                scanf("%d",&item);
+                if(f==-1)
+                        f=r=0;
+                else
+                        r=(r+1)%max;
+
+                a[r]=item;
+        }
 }
 
-int del()
-{
-	if(f==-1)
-	   printf("## Queue Is Empty.");
-	else if(f==r)
-	{
-		f=r=-1;
-	}	
-	else
-	{
-		(f=(f+1)%max);
-		
-	}
-} 
-
-int display()
+void delection()
 {
         if(f==-1)
-            printf("\n## Queue Is Empty.");
+                printf("## Queue Is Empty.");
         else
-        {       
-		    for(i=f;i<r;i++)
-                printf("\nPosition:%d           Value:%d",i,a[i]);
+        {
+                if(f==r)
+                        f=r=-1;
+                else
+                        f=(f+1)%max;
         }
-                printf("\n");
 }
 
 
+void display()
+{
+        if(f!=-1)
+        {
+                for(i=f;i!=r;i=(i+1)%max)
+                {
+                        printf("\n Possition:%d         Value:%d",i,a[i]);
+                }
 
+        else
+
+                printf("## Queue Is Empty.");
+        }
+}
 
 
 int main()
@@ -66,26 +58,26 @@ int main()
         while(1)
         {
                 printf("\n##### Queue Main Manu #####\n");
-                printf("\n1.Insertion. \n2.Delection. \n3.Display \n4.Exit.\n");
+                printf("\n1.Insertion. \n2.Delet \n3.Display \n");
                 printf("\nEnter Your Choice: ");
                 scanf("%d",&choice);
         switch(choice)
         {
                 case 1:
-                insert();
+                insertion();
                 break;
 
                 case 2:
-                del();
+                delection();
                 break;
 
                 case 3:
                 display();
                 break;
 
-                case 4:
-		return 0;
-                break;
+                //case 4:
+                //return 0;
         }
         }
 }
+
